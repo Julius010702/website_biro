7511802a9618403e82768243da4cd94316706daa
Julius <juliusbungadjai@gmail.com>
2026-04-25 23:59:53 +0800

    informasi FIX BARU

---
+++ app/(main)/ppid/informasi-publik/page.tsx
@@ -0,0 +1,6 @@
+// app/(main)/ppid/informasi-publik/page.tsx
+import { redirect } from 'next/navigation'
+
+export default function InformasiPublikPage() {
+  redirect('https://ppid.nttprov.go.id/')
+}
